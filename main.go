da1b16bb07a66d53a2e0164061a06ad7496ac5e4
Camilo Muñoz <jcamilomunozz@gmail.com>
2018-05-17 10:05:00 -0500

    Add arg for dir path

--- main.go
+++ main.go
@@ -8,30 +8,29 @@ import (
 
 func main() {
 	argsWithoutProg := os.Args[1:]
-	// TODO: Check args length
-	filename, newname := argsWithoutProg[0], argsWithoutProg[1]
+	var filename, newname string
+	argsLength := len(argsWithoutProg)
+	if argsLength == 2 {
+		filename, newname = argsWithoutProg[0], argsWithoutProg[1]
+	} else if argsLength == 3 {
+		filename, newname = argsWithoutProg[1], argsWithoutProg[2]
+	}
 
 	//dir := "dir/to/walk"
-	//dir := "sample"
 	dir := "."
-	//subDirToSkip := "skip" // dir/to/walk/skip
 
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", dir, err)
 			return err
 		}
-		/* if info.IsDir() && info.Name() == subDirToSkip {
-			fmt.Printf("skipping a dir without errors: %+v \n", info.Name())
-			return filepath.SkipDir
-		} */
 		if !info.IsDir() && info.Name() == filename {
-			baseDir, _ := filepath.Split(path)
-			err := os.Rename(path, baseDir+newname)
+			dir := filepath.Dir(path)
+			err := os.Rename(path, filepath.Join(dir, newname))
 			if err != nil {
 				panic(err)
 			}
-			fmt.Printf("File \"%s\" in \"%s\" renamed to \"%s\" correctly.\n", filename, baseDir, newname)
+			fmt.Printf("File \"%s\" in \"%s\" renamed to \"%s\" correctly.\n", filename, dir, newname)
 		}
 		return nil
 	})
